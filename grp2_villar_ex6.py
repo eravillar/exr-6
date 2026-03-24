86813334bdb7170169f542d6dda365015527a42d
evillar@paterostechnologicalcollege.edu.ph <evillar@paterostechnologicalcollege.edu.ph>
2026-03-24 12:35:38 +0800

    Create grp2_villar_ex6.py

---
+++ grp2_villar_ex6.py
@@ -0,0 +1,33 @@
+# Script that runs a condition in an if statement
+age = 18  # Use = instead of -
+
+print("Is age 18 or above?")
+print(age >= 18)  # This will print True or False
+
+if age >= 18:
+    print("Condition is True")
+else:
+    print("Condition is False")
+
+# Script that has List, Tuple, Set, and Dictionary
+# Using string, int, and boolean values in each collection
+
+# List
+my_list = ["yellow", 10, True]
+print("List:", my_list)
+
+# Tuple
+my_tuple = ("hotdog", 16, True)
+print("Tuple:", my_tuple)
+
+# Set
+my_set = {"Milk", 7, False}
+print("Set:", my_set)
+
+# Dictionary
+my_dict = {
+    "name": "Era",
+    "score": 90,
+    "passed": True
+}
+print("Dictionary:", my_dict
